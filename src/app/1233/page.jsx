619ebf403c068d43ed50959227cc946d09d141d1
Orgits <anubhav@orgits.in>
2024-12-12 01:23:45 +0530

    ok

--- src/app/1233/page.jsx
+++ src/app/1233/page.jsx
@@ -1,31 +1,31 @@
 import LayoutStyle7 from '@/components/Layouts/LayoutStyle7';
-import AboutStyle2 from '@/components/about/AboutStyle2';
 import PartnerStyle1 from '@/components/partner/PartnerStyle1';
+import PriceStyle1 from '@/components/price/PriceStyle1';
 import ProcessStyle1 from '@/components/process/ProcessStyle1';
+import BlogStyle1 from '@/components/blog/BlogStyle1';
 import RequestCallStyle1 from '@/components/request/RequestCallStyle1';
-import ServicesStyle2 from '@/components/services/ServicesStyle2';
+import ServicesStyle1 from '@/components/services/ServicesStyle1';
 import TeamStyle1 from '@/components/team/TeamStyle1';
-import TestimonialStyle1 from '@/components/testimonial/TestimonialStyle1';
 import React from 'react';
 
 export const metadata = {
     title: "SJVJ & CO. - Services"
 }
 
-const Service2Page = () => {
+const ServicesPage = () => {
     return (
         <>
-            <LayoutStyle7 breadCrumb="Service-2" title="Our Services">
-                <AboutStyle2 />
-                <ServicesStyle2 />
-                <ProcessStyle1 />
+            <LayoutStyle7 breadCrumb="services" title="Our Services">
+                <ServicesStyle1 />
+                {/* <ProcessStyle1 /> */}
                 <PartnerStyle1 sectionClass="default-padding bg-gray" />
-                <TeamStyle1 teamTitle={true} />
+                {/* <TeamStyle1 teamTitle={true} /> */}
                 <RequestCallStyle1 />
-                <TestimonialStyle1 />
+                <PriceStyle1 priceTitle={true} sectionClass="primary" />
+                <BlogStyle1 sectionClass="bg-gray" />
             </LayoutStyle7>
         </>
     );
 };
 
-export default Service2Page;
+export default ServicesPage;
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,29 +1,31 @@
 import LayoutStyle7 from '@/components/Layouts/LayoutStyle7';
+import AboutStyle2 from '@/components/about/AboutStyle2';
 import PartnerStyle1 from '@/components/partner/PartnerStyle1';
-import PriceStyle1 from '@/components/price/PriceStyle1';
 import ProcessStyle1 from '@/components/process/ProcessStyle1';
 import RequestCallStyle1 from '@/components/request/RequestCallStyle1';
-import ServicesStyle1 from '@/components/services/ServicesStyle1';
+import ServicesStyle2 from '@/components/services/ServicesStyle2';
 import TeamStyle1 from '@/components/team/TeamStyle1';
+import TestimonialStyle1 from '@/components/testimonial/TestimonialStyle1';
 import React from 'react';
 
 export const metadata = {
     title: "SJVJ & CO. - Services"
 }
 
-const ServicesPage = () => {
+const Service2Page = () => {
     return (
         <>
-            <LayoutStyle7 breadCrumb="services" title="Our Services">
-                <ServicesStyle1 />
-                <ProcessStyle1 />
+            <LayoutStyle7 breadCrumb="Services" title="Our Services">
+                <AboutStyle2 />
+                <ServicesStyle2 />
+                {/* <ProcessStyle1 /> */}
                 <PartnerStyle1 sectionClass="default-padding bg-gray" />
                 <TeamStyle1 teamTitle={true} />
                 <RequestCallStyle1 />
-                <PriceStyle1 priceTitle={true} sectionClass="primary" />
+                <TestimonialStyle1 />
             </LayoutStyle7>
         </>
     );
 };
 
-export default ServicesPage;
+export default Service2Page;
